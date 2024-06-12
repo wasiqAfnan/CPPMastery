0ba98ecb3d980616fd878d62392290ddb1f367d9
afnan <wansari964@yahoo.com>
2024-06-12 13:55:35 +0530

    Added function

--- Basics/08_loops.cpp
+++ Basics/08_loops.cpp
@@ -32,5 +32,30 @@ int main(){
         cout << k << endl;
         k--;
     } while(k >= 10);
+
+    /* Break Statemnt */
+    // Break keyword is used to jump out of the loop if particular condition meets
+    cout << "\n Break Statements\n";
+    for (int i = 0; i < 5; i++)
+    {
+        if(i == 3){
+            cout << "Break Triggers\n";
+            break;
+        }
+        cout << i+1 << endl;
+    }
+
+    /* Continue Statemnt */
+    // Continue keyword is used to skip some iteration of the loop if particular condition meets
+    cout << "\n Continue Statements\n";
+    for (int i = 0; i < 5; i++)
+    {
+        if(i == 3){
+            cout << "Continue Triggers\n";
+            continue;
+        }
+        cout << i+1 << endl;
+    }
+    
     return 0;
 }
